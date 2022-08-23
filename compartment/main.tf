b2e421cc5913e7e68a6f120f52119ce28e20b34c
Daniel Pro <daniel.procopio@gmail.com>
2022-08-23 18:15:24 +0200

    DP - Added tags

--- compartment/main.tf
+++ compartment/main.tf
@@ -4,5 +4,7 @@ resource "oci_identity_compartment" "identity_compartment" {
   name           = lookup(each.value, "name", each.key)
   description    = lookup(each.value, "description", each.key)
   enable_delete  = lookup(each.value, "enable_delete", false)
+  freeform_tags  = lookup(each.value, "freeform_tags", null)
+  defined_tags   = lookup(each.value, "defined_tags", null)
 }
 
