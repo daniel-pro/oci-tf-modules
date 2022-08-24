9459d7016d2d85a3f9adc04ba7e937a4d12bb18c
Daniel Pro <daniel.procopio@gmail.com>
2022-08-24 17:09:36 +0200

    DP - Fixed cosmetic issue

--- compute-instance/outputs.tf
+++ compute-instance/outputs.tf
@@ -4,7 +4,7 @@ output "instance_all_attributes" {
 }
 
 output "instance_all_input_attributes" {
-   description = "all attributes of created compute instances"
+   description = "all input attributes of created compute instances"
    value       = { for k, v in var.compute_instances : k => v }
 }
 
