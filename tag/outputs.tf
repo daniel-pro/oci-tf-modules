02ad58648250536c12610f3bedcbf6292daacf3a
Daniel Pro <daniel.procopio@gmail.com>
2022-09-15 12:04:15 +0200

    DP - Added missing identity_tag outputs

--- tag/outputs.tf
+++ tag/outputs.tf
@@ -7,3 +7,13 @@ output "tag_namespace_all_attributes" {
   description = "all attributes of created tag namespaces"
   value       = { for k, v in oci_identity_tag_namespace.tag_namespace : k => v }
 }
+
+output "identity_tag_all_input_attributes" {
+  description = "all input attributes of created identity tags"
+  value       = { for k, v in var.identity_tags : k => v }
+}
+
+output "identity_tag_all_attributes" {
+  description = "all attributes of created tag namespaces"
+  value       = { for k, v in oci_identity_tag.identity_tag : k => v }
+}
