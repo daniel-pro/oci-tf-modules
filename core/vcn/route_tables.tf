e1544a28d790461f215cb2285a8e0ceb905e8946
Daniel Pro <daniel.procopio@gmail.com>
2023-07-14 17:16:01 +0200

    DP - Removed default description in route table rules

--- core/vcn/route_tables.tf
+++ core/vcn/route_tables.tf
@@ -15,7 +15,7 @@ resource "oci_core_route_table" "route_table" {
       destination       = route_rules.value.destination_type == "CIDR_BLOCK" ? route_rules.value.destination : (route_rules.value.destination_type == "SERVICE_CIDR_BLOCK" ? lookup(data.oci_core_services.all_oci_services[route_rules.value.network_entity].services[0], "cidr_block") : route_rules.value.destination)
       destination_type  = route_rules.value.destination_type
       network_entity_id = can(route_rules.value.network_entity_id) == true ? route_rules.value.network_entity_id : can(oci_core_nat_gateway.nat_gateway[route_rules.value.network_entity]) == true ? oci_core_nat_gateway.nat_gateway[route_rules.value.network_entity].id : (can(oci_core_internet_gateway.internet_gateway[route_rules.value.network_entity]) == true ? oci_core_internet_gateway.internet_gateway[route_rules.value.network_entity].id : (can(oci_core_service_gateway.service_gateway[route_rules.value.network_entity]) == true ? oci_core_service_gateway.service_gateway[route_rules.value.network_entity].id : null))
-      description       = lookup(route_rules.value, "description", "Terraform managed rule")
+      description       = lookup(route_rules.value, "description", null)
     }
   }
 }
