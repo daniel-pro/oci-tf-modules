6daf91856bc8cee6305d10b852d220ba16ff632c
Francesco D'Anzi <f.danzi@mediamenteconsulting.it>
2025-09-11 12:17:08 +0200

    FDA - Added Autonomous Recovery Service module

---
+++ database/recovery/outputs.tf
@@ -0,0 +1,15 @@
+# output "oci_database_db_system_all_attributes" {
+#   description = "all attributes of created database system"
+#   value       = { for k, v in oci_database_db_system.db_system : k => v }
+#   sensitive = true
+# }
+
+# output "oci_database_db_system_all_input_attributes" {
+#   description = "all input attributes of created database system"
+#   value       = { for k, v in var.db_systems : k => v }
+# }
+
+# output "oci_databases_on_all_db_systems" {
+#   description = "all dbs created during the DB system creation"
+#   value       = data.oci_database_databases.databases
+# }
