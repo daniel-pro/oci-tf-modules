b48ab659252bdaf4b7430c1d24a7d2353a40d9de
Francesco D'Anzi <f.danzi@mediamenteconsulting.it>
2025-12-04 06:07:31 +0100

    FDA - Set TF provider to v7.21.0 for exacs module

--- database/exacs/versions.tf
+++ database/exacs/versions.tf
@@ -2,7 +2,9 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = ">=4.86.0"
+      #version = ">=4.86.0"
+      version = "7.21.0"
+
     }
   }
   required_version = ">= 1.2.4"
