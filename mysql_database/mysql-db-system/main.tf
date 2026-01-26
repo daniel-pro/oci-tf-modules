1e79bfff1197d3674d2b3f038b9b6bbde2142679
Francesco D'Anzi <f.danzi@mediamenteconsulting.it>
2026-01-26 09:44:31 +0100

    FDA - Temporarily comment out mysql-db-system fields with errors

--- mysql_database/mysql-db-system/main.tf
+++ mysql_database/mysql-db-system/main.tf
@@ -29,10 +29,10 @@ resource "oci_mysql_mysql_db_system" "mysql_db_system" {
       defined_tags = lookup(each.value.backup_policy, "defined_tags", null)
       freeform_tags = lookup(each.value.backup_policy, "freeform_tags", null)
       is_enabled = lookup(each.value.backup_policy, "is_enabled", null)
-      pitr_policy {
-          #Required
-          is_enabled = lookup(each.value.backup_policy.pitr_policy, "is_enabled", null)
-      }
+      # pitr_policy {
+      #     #Required
+      #     is_enabled = lookup(each.value.backup_policy.pitr_policy, "is_enabled", null)
+      # }
       retention_in_days = lookup(each.value.backup_policy, "retention_in_days", null)
       soft_delete = lookup(each.value.backup_policy, "soft_delete", null)
       window_start_time = lookup(each.value.backup_policy, "window_start_time", null)
@@ -40,10 +40,10 @@ resource "oci_mysql_mysql_db_system" "mysql_db_system" {
     configuration_id = lookup(each.value, "configuration_id", null)
     crash_recovery = lookup(each.value, "crash_recovery", null)
     dynamic customer_contacts {
-      for_each = lookup(each.value, "customer_contacts", {})
+      for_each = lookup(each.value, "customer_contact_emails", {})
       content {
         #Required
-        email = customer_contacts.value.email
+        email = customer_contacts.value
       }
     }
     data_storage {
@@ -63,44 +63,44 @@ resource "oci_mysql_mysql_db_system" "mysql_db_system" {
     }
     description = lookup(each.value, "description", null)
     display_name = lookup(each.value, "display_name", each.key)
-    encrypt_data {
-        #Required
-        key_generation_type = lookup(each.value.encrypt_data, "key_generation_type", null)
+    # encrypt_data {
+    #     #Required
+    #     key_generation_type = lookup(each.value.encrypt_data, "key_generation_type", null)
 
-        #Optional
-        key_id = lookup(each.value.encrypt_data, "key_id", null)
-    }
+    #     #Optional
+    #     key_id = lookup(each.value.encrypt_data, "key_id", null)
+    # }
     fault_domain = lookup(each.value, "fault_domain", null)
     freeform_tags = lookup(each.value, "freeform_tags", null)
     hostname_label = lookup(each.value, "hostname_label", null)
     ip_address = lookup(each.value, "ip_address", null)
     is_highly_available = lookup(each.value, "is_highly_available", null)
-    maintenance {
-      #Required
-      window_start_time = lookup(each.value.maintenance, "window_start_time", null)
+    # maintenance {
+    #   #Required
+    #   window_start_time = lookup(each.value.maintenance, "window_start_time", null)
 
-      #Optional
-      maintenance_schedule_type = lookup(each.value.maintenance, "maintenance_schedule_type", null)
-      version_preference = lookup(each.value.maintenance, "version_preference", null)
-      version_track_preference = lookup(each.value.maintenance, "version_track_preference", null)
-    }
+    #   #Optional
+    #   maintenance_schedule_type = lookup(each.value.maintenance, "maintenance_schedule_type", null)
+    #   version_preference = lookup(each.value.maintenance, "version_preference", null)
+    #   version_track_preference = lookup(each.value.maintenance, "version_track_preference", null)
+    # }
     nsg_ids = lookup(each.value, "nsg_ids", null)
     port = lookup(each.value, "port", null)
     port_x = lookup(each.value, "port_x", null)
-    read_endpoint {
-      #Optional
-      exclude_ips = lookup(each.value.read_endpoint, "exclude_ips", null)
-      is_enabled = lookup(each.value.read_endpoint, "is_enabled", null)
-      read_endpoint_hostname_label = lookup(each.value.read_endpoint, "read_endpoint_hostname_label", null)
-      read_endpoint_ip_address = lookup(each.value.read_endpoint, "read_endpoint_ip_address", null)
-    }
-    rest {
-      #Required
-      configuration = lookup(each.value.rest, "configuration", null)
+    # read_endpoint {
+    #   #Optional
+    #   exclude_ips = lookup(each.value.read_endpoint, "exclude_ips", null)
+    #   is_enabled = lookup(each.value.read_endpoint, "is_enabled", null)
+    #   read_endpoint_hostname_label = lookup(each.value.read_endpoint, "read_endpoint_hostname_label", null)
+    #   read_endpoint_ip_address = lookup(each.value.read_endpoint, "read_endpoint_ip_address", null)
+    # }
+    # rest {
+    #   #Required
+    #   configuration = lookup(each.value.rest, "configuration", null)
 
-      #Optional
-      port = lookup(each.value.rest, "port", null)
-    }
+    #   #Optional
+    #   port = lookup(each.value.rest, "port", null)
+    # }
     secure_connections {
         #Required
         certificate_generation_type = lookup(each.value.secure_connections, "certificate_generation_type", null)
@@ -109,17 +109,17 @@ resource "oci_mysql_mysql_db_system" "mysql_db_system" {
         certificate_id = lookup(each.value.secure_connections, "certificate_id", null)
     }
     security_attributes = lookup(each.value, "security_attributes", null)
-    source {
-        #Required
-        source_type = lookup(each.value.source, "source_type", null)
+    # source {
+    #     #Required
+    #     source_type = lookup(each.value.source, "source_type", null)
 
-        #Optional
-        ## Required if source_type is BACKUP
-        backup_id = lookup(each.value.source, "backup_id", null)
-        ## Required if source_type is PITR
-        db_system_id = lookup(each.value.source, "db_system_id", null)
-        recovery_point = lookup(each.value.source, "recovery_point", null)
-        ## Required if source_type is IMPORTURL
-        source_url = lookup(each.value.source, "source_url", null)
-    }
+    #     #Optional
+    #     ## Required if source_type is BACKUP
+    #     backup_id = lookup(each.value.source, "backup_id", null)
+    #     ## Required if source_type is PITR
+    #     db_system_id = lookup(each.value.source, "db_system_id", null)
+    #     recovery_point = lookup(each.value.source, "recovery_point", null)
+    #     ## Required if source_type is IMPORTURL
+    #     source_url = lookup(each.value.source, "source_url", null)
+    # }
 }
